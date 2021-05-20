672f9d3c0ae2e3af77003cf7d1a8949768bc447f
andrefranciscol <andre.leite@talkdesk.com>
2021-05-20 10:08:00 +0100

    Add intermediary topic

--- favourite-colour/src/main/java/FavouriteColorApp.java
+++ favourite-colour/src/main/java/FavouriteColorApp.java
@@ -1,6 +1,7 @@
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
@@ -17,32 +18,36 @@ public class FavouriteColorApp {
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
 
         KStreamBuilder builder = new KStreamBuilder();
         // 1 - stream from Kafka
 
         KStream<String, String> stream = builder.stream(Serdes.String(), Serdes.String(), "favourite-colour-input");
         // KTable since user can change opinion about is favourite color - allows update
-        KStream<String, String> favoriteColorsCount = stream
-                // 2 - map values to lowercase
-                .mapValues(colour -> colour.toLowerCase())
-                // 3 - filter admissible colors
-                .filter((user_id, colour) -> Arrays.asList("blue", "green").contains(colour));
-                // can be alternatively written as:
-                // .mapValues(String::toLowerCase)
-                // 3 - flatmap values split by space
-                //.flatMapValues(textLine -> Arrays.asList(textLine.split("\\W+")))
-                // 4 - select key to apply a key (we discard the old key)
-                //.selectKey((key, word) -> word)
-                // 5 - group by key before aggregation
-                //.groupByKey()
-                // 6 - count occurrences
-                //.count("Counts");
+        KStream<String, String> favoriteColoursUser = stream
+                // 2 - filter only if contains 1 comma
+                .filter((key,value) -> value.contains(","))
+                // 3 - assign key
+                .selectKey((key,value) -> value.split(",")[0].toLowerCase())
+                // 4 - assign value
+                .mapValues(value -> value.split(",")[1].toLowerCase())
+                // 5 - filter admissible colors
+                .filter((user_id, colour) -> Arrays.asList("blue", "green", "red").contains(colour));
+                // 6 - group by key before aggregation
+
+        favoriteColoursUser.to("favourite-colours-users");
+
+        KTable<String, String> streamColours = builder.table(Serdes.String(), Serdes.String(), "favourite-colours-users");
+        KTable<String, Long> coloursCount = streamColours
+                .groupBy((user,colour) -> new KeyValue<>(colour, user))
+                .count("ColoursCount");
 
         // 7 - to in order to write the results back to kafka
-        favoriteColorsCount.to("favourite-colour-output");
+        coloursCount.to(Serdes.String(), Serdes.Long(),"favourite-colour-output");
 
         KafkaStreams streams = new KafkaStreams(builder, config);
+        streams.cleanUp();
         streams.start();
 
 
